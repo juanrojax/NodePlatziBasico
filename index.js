1801278ea10949df02f6533b94296f2deac6962b
Juan Diego <juanrojax05@gmail.com>
2019-08-04 11:19:25 -0500

    Node Modulos externos required

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+//importar modulo cowsay
+const cowsay = require("cowsay");
+
+//Usar modulo importado cowsay
+//www.npmjs.com/package/cowsay
+console.log(cowsay.say({
+    text : "Hola Soy Diego",
+    e: "o0",
+    T: ".l."
+}));
