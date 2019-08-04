6a278ea80d385f29d81c26b2f0fc637af0376591
Juan Diego <juanrojax05@gmail.com>
2019-08-04 10:55:39 -0500

    Coemmit node require modulos locales

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-/**
- * *************
- * Comience aqui
- * *************
- */
+const math = require('./math');
+const greet = require('./greetings');
+const hello = require('./greetings/hello');
+
+console.log(math.add(1,2));
+console.log(math.divide(4,2));
+console.log(math.multiply(1,2));
+console.log(math.substract(1,2));
+
+//Ctrl f5 consola de depuracion
+
+console.log(greet.greet("JuanDi"));
+console.log(hello.sayHello("Juan Di"));
+
+
+
