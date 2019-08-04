0f086cecbbb76b6051619e85207b6685c096378d
Juan Diego <juanrojax05@gmail.com>
2019-08-04 11:56:42 -0500

    Commit NodeJS Callbacks y Promesas

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+console.log(call.sync("Diego"));
+
+
+call.withCallback("Diego",call.sync);
+
+call.withPromise("JuanDi")
+    .then(name=>{console.log(name)})
