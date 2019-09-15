e26f09f742cc257c21f998841935e203d0d52796
Juan Diego <juanrojax05@gmail.com>
2019-09-15 16:56:14 -0500

    Clase17 lectura y Escritura Node.js

--- apuntes.php
+++ apuntes.php
@@ -2,3 +2,6 @@ hemos trabajado require() y module.export tambien hemos manejado modulos externo
 
 CLASE 17
 FileSystem: lEctura y Escritura de archivos en Nodejs
+Se utilizo la Rama clase-node-fs-base 
+se utilizo modulo nativo de node fs para lectura de y escitura de archivos 
+utilizamos funciones como readFile y writeFile
