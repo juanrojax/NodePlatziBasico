cfd89e42336346429ac5aee4a5c08af7ab947198
Juan Diego <juanrojax05@gmail.com>
2019-09-15 16:52:19 -0500

    Escritura y lectura de archivos Nodejs

---
+++ apuntes.php
@@ -0,0 +1,4 @@
+hemos trabajado require() y module.export tambien hemos manejado modulos externos con NPM y require()
+
+CLASE 17
+FileSystem: lEctura y Escritura de archivos en Nodejs
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require("fs")
+const ops = require("./src/fileops");
+
+let incValue;
+
+fs.readFile("nodejsbasico/resources/number.txt","utf8", (err,text)=>{
+    if(err) throw err;
+    const numbers = text.split("\n").map(n=>Number(n));
+    console.log(numbers);
+    console.log(ops.incrementValues(numbers))
+    incValue = ops.incrementValues(numbers)
+
+    fs.writeFile("nodejsbasico/resources/numbernew.txt", incValue.join("\n"),(err,result)=>{
+        if(err) throw err;
+    })
+});
+
